5db829751cf8c7d11e45e1b16f0a5b9882d15884
Mikey Ying <mikeyinguk@gmail.com>
2023-08-30 20:46:05 +0100

    change release date to date in migration

--- backend/database/migrations/2023_08_30_192227_create_games_table.php
+++ backend/database/migrations/2023_08_30_192227_create_games_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('games', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('release_date')->nullable();
+            $table->date('release_date')->nullable();
             $table->string('developer')->nullable();
             $table->text('description')->nullable();
             $table->timestamps();
