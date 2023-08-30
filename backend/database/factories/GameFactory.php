7cf98b26a1aec0d63ca8df8cca9753181707604b
Mikey Ying <mikeyinguk@gmail.com>
2023-08-30 23:43:36 +0100

    change fake game name

--- backend/database/factories/GameFactory.php
+++ backend/database/factories/GameFactory.php
@@ -17,7 +17,7 @@ class GameFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->sentence(1),
+            'name' => fake()->words(random_int(1, 3), true),
             'release_date' => fake()->date(),
             'developer' => fake()->company(),
             'description' => fake()->paragraph(),
