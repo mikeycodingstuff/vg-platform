8ece7585c575dc020325ba6375b7546e9e4054c1
Mikey Ying <mikeyinguk@gmail.com>
2023-08-31 23:14:20 +0100

    add validation to console command clean up code

--- backend/app/Console/Commands/CreateUser.php
+++ backend/app/Console/Commands/CreateUser.php
@@ -4,6 +4,8 @@ namespace App\Console\Commands;
 
 use App\Models\User;
 use Illuminate\Console\Command;
+use Illuminate\Contracts\Support\MessageBag;
+use Illuminate\Support\Facades\Validator;
 
 class CreateUser extends Command
 {
@@ -31,16 +33,60 @@ class CreateUser extends Command
             'email' => $this->argument('email'),
             'password' => $this->argument('password'),
         ];
-        
-        $user = User::create($data);
+
+        $validator = Validator::make($data, [
+            'name' => 'string|max:255',
+            'email' => 'string|unique:users|max:255',
+            'password' => 'string|max:255',
+        ]);
+
+        if ($validator->fails()) {
+            $this->comment(static::formatErrorsForConsole(errors: $validator->errors()));
+            exit(1);
+        }
+
+        $validated = $validator->validated();
+
+        $user = User::create($validated);
         $token = $user->createToken('api-token')->plainTextToken;
 
         if ($user) {
-            echo "User $user->id $user->name successfully created. API token: $token";
-            echo PHP_EOL;
-        } else {
-            echo "Could not create user. Please try again with different information.";
-            echo PHP_EOL;
+            $this->comment(static::formatForConsole($user, $token));
         }
+    }
+
+    /**
+     * Formats the given text for a pretty console output.
+     *
+     * @param  string  $quote
+     * @return string
+     */
+    protected static function formatForConsole(User $user, string $token): string
+    {
+        return sprintf(
+            "\n  <options=bold>%s </>\n  %s\n",
+            trim("User $user->id $user->name successfully created."),
+            trim("API token: $token"),
+        );
+    }
+
+    /**
+     * Formats the given errors for a console output.
+     *
+     * @param MessageBag $errors
+     * @return string
+     */
+    protected static function formatErrorsForConsole(MessageBag $errors): string
+    {
+        $errorMessages = "";
+
+        foreach ($errors->all() as $message) {
+            $errorMessages .= "\n  <fg=red>- $message</>";
+        }
+
+        return sprintf(
+            "\n  <options=bold>%s</>\n",
+            trim("Could not create user. Please try again with different information.$errorMessages"),
+        );
     }
 }
