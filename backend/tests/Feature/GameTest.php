f6abeb490e7be2c8c7ed8b2455e85e65baeab695
Mikey Ying <mikeyinguk@gmail.com>
2023-08-31 18:47:53 +0100

    refactor tests

--- backend/tests/Feature/GameTest.php
+++ backend/tests/Feature/GameTest.php
@@ -6,29 +6,25 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 uses(RefreshDatabase::class);
 
 describe('index route', function () {
-    it('returns a successful response', function () {
+    it('can fetch all games', function () {
+        $games = Game::factory()->count(50)->create();
+
         $response = $this->get('/games');
 
-        $response->assertStatus(200);
+        $response->assertStatus(200)->assertJson($games->toArray());
     });
 });
 
 describe('show route', function () {
-    it('returns a successful response', function () {
+    it('can fetch a game', function () {
         $game = Game::factory()->create();
+
         $response = $this->get("/games/$game->id");
 
-        $response->assertStatus(200)
-            ->assertJson([
-                'id' => $game->id,
-                'name' => $game->name,
-                'release_date' => $game->release_date,
-                'description' => $game->description,
-                'developer' => $game->developer,
-            ]);
+        $response->assertStatus(200)->assertJson($game->toArray());
     });
 
-    it('returns a 404 for a nonexistent resource', function () {
+    it('returns a 404 for a nonexistent game', function () {
         $response = $this->get('/games/1');
         $response->assertStatus(404);
     });
